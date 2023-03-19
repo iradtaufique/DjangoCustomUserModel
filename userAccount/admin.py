b3365cf22a76c0895cb0bd703815c07f5d0a0e3b
iradtaufique <iradukunda.ta@gmail.com>
2023-03-19 02:04:06 -0700

    customize admin site

--- userAccount/admin.py
+++ userAccount/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 from userAccount.models import Account
 
-# Register your models here.
-admin.site.register(Account)
+
+class AccountAdmin(UserAdmin):
+    """Customize admin site to look better"""
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_admin')
+    search_fields = ('email', 'username')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
+
